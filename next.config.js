08ac13d7b631ae34cb7e663f8bdcafc286378103
dev-back55 <ahabitu@gmail.com>
2022-11-12 19:28:16 -0300

    Feact/second commit fixture-login-filters-order

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+require('dotenv').config();
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    API_URL: process.env.API_URL
+  }
 }
 
 module.exports = nextConfig
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+export async function getServerSideProps() {
+  const { API_URL } = process.env
+  const res = fetch(`${API_URL}/api/partidos`)
+  const data = await res.json()
+
+  return {
+    props: {
+      partidos: data.attributes
+    }
+  }
 }
