60dc56ac79e984c5196730320c80958e59616350
dev-back55 <ahabitu@gmail.com>
2022-11-16 22:59:10 -0300

    Feact/Image domain  auth in  nextjs.config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,10 @@ const nextConfig = {
   swcMinify: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
-  }
+  },
+  images: {
+    domains: ['www.worldometers.info', 't3.ftcdn.net', 't4.ftcdn.net'],
+  },
 }
 
 module.exports = nextConfig
